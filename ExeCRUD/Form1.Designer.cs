403bbd8fb174d4d8e1e214c2a760dc2aa321bf9c
ArhabZuhan <Zuhanarsa@gmail.com>
2022-06-15 16:55:21 +0700

    Add project files.

---
+++ ExeCRUD/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace ExeCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdAdd = new System.Windows.Forms.Button();
+            this.CmdSave = new System.Windows.Forms.Button();
+            this.CmdDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtCode = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtGender = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CmdAdd
+            // 
+            this.CmdAdd.Location = new System.Drawing.Point(134, 304);
+            this.CmdAdd.Name = "CmdAdd";
+            this.CmdAdd.Size = new System.Drawing.Size(75, 23);
+            this.CmdAdd.TabIndex = 0;
+            this.CmdAdd.Text = "ADD";
+            this.CmdAdd.UseVisualStyleBackColor = true;
+            // 
+            // CmdSave
+            // 
+            this.CmdSave.Location = new System.Drawing.Point(281, 304);
+            this.CmdSave.Name = "CmdSave";
+            this.CmdSave.Size = new System.Drawing.Size(75, 23);
+            this.CmdSave.TabIndex = 1;
+            this.CmdSave.Text = "SAVE";
+            this.CmdSave.UseVisualStyleBackColor = true;
+            // 
+            // CmdDelete
+            // 
+            this.CmdDelete.Location = new System.Drawing.Point(423, 304);
+            this.CmdDelete.Name = "CmdDelete";
+            this.CmdDelete.Size = new System.Drawing.Size(75, 23);
+            this.CmdDelete.TabIndex = 2;
+            this.CmdDelete.Text = "DELETE";
+            this.CmdDelete.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "NIM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(174, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "NAMA";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(168, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "JENIS KELAMIN";
+            // 
+            // TxtCode
+            // 
+            this.TxtCode.Cursor = System.Windows.Forms.Cursors.Default;
+            this.TxtCode.Location = new System.Drawing.Point(303, 93);
+            this.TxtCode.Name = "TxtCode";
+            this.TxtCode.Size = new System.Drawing.Size(100, 22);
+            this.TxtCode.TabIndex = 6;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(303, 153);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtGender
+            // 
+            this.txtGender.Location = new System.Drawing.Point(303, 215);
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(100, 22);
+            this.txtGender.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtGender);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.TxtCode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CmdDelete);
+            this.Controls.Add(this.CmdSave);
+            this.Controls.Add(this.CmdAdd);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdAdd;
+        private System.Windows.Forms.Button CmdSave;
+        private System.Windows.Forms.Button CmdDelete;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtCode;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtGender;
+    }
+}
+
